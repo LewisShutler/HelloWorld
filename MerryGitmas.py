2d408512759d4f2f3e1667f7873a93b0d7084144
t-cann <tom.cann@live.co.uk>
2020-12-23 15:46:33 +0000

    Update MerryGitmas.py

--- MerryGitmas.py
+++ MerryGitmas.py
@@ -5,10 +5,8 @@ Created on Wed Dec 23 15:10:45 2020
 @author: Lewis Shutler
 @author: Thomas Cann
 """
-def main():
-    print("Merry Gitmas!")
-    print_tree(7)
 
+    
 def print_segment(n, total_width):
     for size in range(1, n+1, 2):
         print((size * "*").center(total_width))
@@ -17,5 +15,5 @@ def print_tree(size):
     for i in range(3, size+1, 2):
         print_segment(i, size)
 
-if __name__=='__main__':
-    main()
+print("Merry Gitmas!")
+print_tree(7)
