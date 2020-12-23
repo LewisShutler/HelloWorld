93c855b143b28c81e6ff4c1b1f97f6438e0b3717
t-cann <tom.cann@live.co.uk>
2020-12-23 15:36:08 +0000

    Update MerryGitmas.py
    Add Christmas Tree to Message
    
    Co-Authored-By: lewisshutler <76526956+lewisshutler@users.noreply.github.com>

--- MerryGitmas.py
+++ MerryGitmas.py
@@ -4,5 +4,17 @@ Created on Wed Dec 23 15:10:45 2020
 
 @author: Lewis Shutler
 """
+def main():
+    print("Merry Gitmas!")
+    print_tree(7)
+    
+def print_segment(n, total_width):
+    for size in range(1, n+1, 2):
+        print((size * "*").center(total_width))
 
-print("Merry Gitmas!")
+def print_tree(size):
+    for i in range(3, size+1, 2):
+        print_segment(i, size)
+
+if __name__=='__main__':
+    main()
