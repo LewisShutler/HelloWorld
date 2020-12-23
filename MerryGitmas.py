db9e36622152abbe69a51466fd1b5f576b4493ac
LewisShutler <lewisshutler@hotmail.co.uk>
2020-12-23 16:07:49 +0000

    Update MerryGitmas.py
    Small corrections and moving code around.

--- MerryGitmas.py
+++ MerryGitmas.py
@@ -10,12 +10,11 @@ Created on Wed Dec 23 15:10:45 2020
 from datetime import datetime
 
 def main():
-    print("Merry Gitmas!")
-    #Time now
     now = datetime.now()
-    christmas = datetime(now.year, 12, 25)
+    print("Merry Gitmas!")
     calculateDays(now)
     print_tree(7)
+    
     
 def print_segment(n, total_width):
     for size in range(1, n+1, 2):
@@ -26,13 +25,14 @@ def print_tree(size):
         print_segment(i, size)
 
 def calculateDays(now):
+    christmas = datetime(now.year, 12, 25)
     difference = christmas.day - now.day
     if difference > 0:
         print(f"It is {difference} days till Christmas!")
     else:
         print("It is Christmas!")
         
-if __name__ = '__main__' :
+if __name__ == '__main__' :
     main()
 
 
